bc459c4110eefb24166a3d58173f6dadb7388bca
Munsky <munsky@engr.colostate.edu>
2023-11-22 20:08:23 -0700

    Update hello_test.py

--- hello_test.py
+++ hello_test.py
@@ -2,3 +2,6 @@ import hello;
 
 def test_hello():
     assert hello.hello_world() == "Hello World!"
+
+def test_helloN():
+    assert hello.hello_world_n(3) == "Hello World! Hello World! Hello World!"
