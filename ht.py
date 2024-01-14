54af0a7b504f9593095582cc0b70f72231000f63
Munsky <munsky@engr.colostate.edu>
2024-01-14 12:40:36 -0700

    Update ht.py

--- ht.py
+++ ht.py
@@ -3,5 +3,5 @@ import hello
 def test_hello():
     assert hello.hello_world() == "Hello World!"
     
-def test_helloN():
+def test_Nhello():
     assert hello.hello_world_n(3) == "Hello World! Hello World! Hello World!"
